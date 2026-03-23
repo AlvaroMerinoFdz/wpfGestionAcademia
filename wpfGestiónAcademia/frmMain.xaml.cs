cc91ee583782d416cc45d8e66f31c1486a588cba
Álvaro Merino Fernández <alvaro.merino.fernandez@gmail.com>
2026-03-23 20:12:45 +0100

    Ventana principal cambiada

--- wpfGestiónAcademia/frmMain.xaml.cs
+++ wpfGestiónAcademia/frmMain.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace wpfGestiónAcademia
 {
     /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
+    /// Lógica de interacción para frmMain.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class frmMain : Window
     {
-        public MainWindow()
+        public frmMain()
         {
             InitializeComponent();
         }
